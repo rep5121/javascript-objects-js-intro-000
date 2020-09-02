896e620d326590155fc3aa5ba06fe81cd411c40a
bold-mixer-0681 <renee.pujol@gmail.com>
2020-09-02 23:43:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist= {'Alison': 'Slowdive'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign(playlist,artistName,songTitle)
+  return Object.assign(playlist,{artistName:songTitle})
   }
